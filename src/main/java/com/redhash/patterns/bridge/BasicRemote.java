5b12cdd8c8a9f39671b2869bd4b73350732c63da
Artem Kudria <artem.kudria@ocado.com>
2024-08-16 22:40:17 +0200

    3: Add Bridge example

---
+++ src/main/java/com/redhash/patterns/bridge/BasicRemote.java
@@ -0,0 +1,43 @@
+package com.redhash.patterns.bridge;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class BasicRemote implements Remote {
+
+    protected final Device device;
+
+    @Override
+    public void power() {
+        System.out.println("Remote: power toggle");
+        if (device.isEnabled()) {
+            device.disable();
+        } else {
+            device.enable();
+        }
+    }
+
+    @Override
+    public void volumeDown() {
+        System.out.println("Remote: volume down");
+        device.setVolume(device.getVolume() - 10);
+    }
+
+    @Override
+    public void volumeUp() {
+        System.out.println("Remote: volume up");
+        device.setVolume(device.getVolume() + 10);
+    }
+
+    @Override
+    public void channelDown() {
+        System.out.println("Remote: channel down");
+        device.setChannel(device.getChannel() - 1);
+    }
+
+    @Override
+    public void channelUp() {
+        System.out.println("Remote: channel up");
+        device.setChannel(device.getChannel() + 1);
+    }
+}
---
+++ src/main/java/com/redhash/patterns/bridge/Device.java
@@ -0,0 +1,20 @@
+package com.redhash.patterns.bridge;
+
+public interface Device {
+
+    boolean isEnabled();
+
+    void enable();
+
+    void disable();
+
+    int getVolume();
+
+    void setVolume(int volume);
+
+    int getChannel();
+
+    void setChannel(int channel);
+
+    void printStatus();
+}
